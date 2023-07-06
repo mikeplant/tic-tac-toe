28ea348e45feb9397505645ebf65638a878ec858
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-07-06 14:54:51 +0100

    Add game, displayHandler, eventHandler, player functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
   const squares = [];
-  const gameBoard = document.querySelector('.game-board');
+  const el = document.querySelector('.game-board');
 
   const init = () => {
     createSquares();
@@ -8,17 +8,94 @@ const gameBoard = (() => {
   }
 
   const createSquares = () => {
-    for (let i = 1; i <= 9; i++) {
-      const square = document.createElement('div');
-      square.classList.add('board-square');
-      square.dataset.sqid = i;
-      squares.push(square);
+    for (let i = 0; i < 9; i++) {
+      const squareEl = document.createElement('div');
+      squareEl.classList.add('board-square');
+      squareEl.dataset.sqid = i;
+      squares.push(squareEl);
     };
   };
 
   const render = () => {
-    squares.forEach(sq => gameBoard.append(sq));
+    squares.forEach(sq => el.append(sq));
+  };
+
+  return {
+    init, 
+    el
+  };
+})();
+
+
+const player = (name, type, token) => {
+  let ownedSquares = [];
+  return {name, type, token, ownedSquares};
+};
+
+
+
+const game = (() => {
+  let players = [];
+  let currentPlayer;
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const init = () => {
+    gameBoard.init();
+    eventHandler.init();
+    createPlayers();
+  };
+
+  const createPlayers = () => {
+    const player1 = player('p1', 'human', 'X');
+    players.push(player1);
+    const player2 = player('p2', 'human', 'O');
+    players.push(player2);
+
+    currentPlayer = player1;
+  }
+
+  const changePlayer = () => {
+    currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0];
+  }
+
+  return {
+    init, 
+    getCurrentPlayer,
+    changePlayer
+  };
+
+})();
+
+
+
+
+const eventHandler = (() => {
+
+  const init = () => {
+    bindEvents();
+  };
+
+  const bindEvents = () => {
+    gameBoard.el.addEventListener('click', e => handleSquareClick(e));
+  };
+
+  const handleSquareClick = (e) => {
+    game.getCurrentPlayer().ownedSquares.push(e.target.dataset.sqid);
+    displayHandler.fillSquare(e);
+    game.changePlayer();
   };
 
   return {init};
+  
 })();
+
+const displayHandler = (() => {
+  const fillSquare = (e) => {
+    e.target.textContent = game.getCurrentPlayer().token;
+  };
+
+  return {fillSquare};
+})();
+
+game.init();
