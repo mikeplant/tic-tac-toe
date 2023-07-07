79e9365c02fdc24e1f85dd2cbca95290a290aa57
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-07-07 16:11:02 +0100

    Change win/draw check to return boolean, add results screen on game end

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const gameBoard = (() => {
   const el = document.querySelector('.game-board');
 
   const init = () => {
+    // resetBoard();
     createSquares();
     render();
   }
@@ -12,11 +13,15 @@ const gameBoard = (() => {
   const createSquares = () => {
     for (let i = 0; i < 9; i++) {
       const squareEl = document.createElement('div');
-      squareEl.classList.add('board-square');
+      squareEl.classList.add('board-square', 'open');
       squareEl.dataset.sqid = i;
       squares.push(squareEl);
     };
   };
+
+  const resetBoard = () => {
+    /////////////////////////////////////////////
+  }
 
   const render = () => {
     squares.forEach(sq => el.append(sq));
@@ -48,9 +53,9 @@ const game = (() => {
   };
 
   const createPlayers = () => {
-    const player1 = player('p1', 'human', 'X');
+    const player1 = player('Player One', 'human', 'X');
     players.push(player1);
-    const player2 = player('p2', 'human', 'O');
+    const player2 = player('Player Two', 'human', 'O');
     players.push(player2);
 
     currentPlayer = player1;
@@ -71,12 +76,13 @@ const game = (() => {
   }
 
   const checkGameOver = () => {
-    checkForWin();
-    checkForDraw();
+    if (isWin() || isDraw()) displayHandler.showResults(winner);
   }
 
-  const checkForWin = () => {
-    players.forEach(p => checkForComboMatch(p));
+  const isWin = () => {
+    let win = false;
+    players.forEach(p => {if (checkForComboMatch(p)) win = true});
+    return win;
   };
 
   const checkForComboMatch = (p) => {
@@ -93,12 +99,12 @@ const game = (() => {
     for (let i = 0; i < winningCombos.length; i++) {
       if (winningCombos[i].every(sq => p.ownedSquares.includes(sq))) {
         winner = p;
-        console.log('win') ////
+        return true;
       }
     }
   };
 
-  const checkForDraw = () => {
+  const isDraw = () => {
     return allSquaresAreOwned() && winner === null;
   };
 
@@ -117,6 +123,7 @@ const game = (() => {
 // EVENT HANDLER MODULE
 
 const eventHandler = (() => {
+  const replayBtn = document.querySelector('.replay-btn');
 
   const init = () => {
     bindEvents();
@@ -124,6 +131,7 @@ const eventHandler = (() => {
 
   const bindEvents = () => {
     gameBoard.el.addEventListener('click', e => handleSquareClick(e));
+    // replayBtn.addEventListener('click', () => gameBoard.init());
   };
 
   const handleSquareClick = (e) => {
@@ -142,15 +150,35 @@ const eventHandler = (() => {
 // DISPLAY HANDLER MODULE
 
 const displayHandler = (() => {
+  const resultsEl = document.querySelector('.results');
+  const resultsTextEl = document.querySelector('.results-text');
+
   const fillSquare = (e) => {
     e.target.textContent = game.getCurrentPlayer().token;
+    e.target.classList.remove('open');
     colourPlayerToken(e);
   };
+
   const colourPlayerToken = (e) => {
     e.target.style.color = (e.target.textContent === 'X') ? '#FFC800' : '#FF8427';
   }
 
-  return {fillSquare};
+  const updateResultText = (winner) => {
+    resultsTextEl.textContent = (winner !== null) ? 
+      `${winner.name} wins!`:
+      `It's a draw!`;
+  }
+
+  const showResults = (winner) => {
+    updateResultText(winner);
+    resultsEl.classList.toggle('show');
+  }
+
+  const hideResults = () => {
+    resultsEl.classList.toggle('show');
+  }
+
+  return {fillSquare, showResults};
 })();
 
 game.init();
